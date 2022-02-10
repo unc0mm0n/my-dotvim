158ea94e58646b5651e44089d0096c011086ee8c
YuvalW <yvw.bor@gmail.com>
2022-02-10 12:50:16 +0100

    oh-my-zsh support

--- setup.sh
+++ setup.sh
@@ -1,8 +1,12 @@
 # Sets the environment up on a new install. Run from my-dotvim dir
 
-# link bash files
-ln -s $PWD/.bash_aliases ~/.bash_aliases
-ln -s $PWD/.bashrc.local ~/.bashrc.local
+if [ ! -z ${ZSH_CUSTOM}]; then
+  ln -s $HOME/my-dotvim/zsh_aliases.zsh $ZSH_CUSTOM/galiases.zsh
+else
+  # link bash files
+  ln -s $PWD/.bash_aliases ~/.bash_aliases
+  ln -s $PWD/.bashrc.local ~/.bashrc.local
+fi
 
 # use precompiled nvim 0.5 
 mkdir -p ~/bin
---
+++ zsh_aliases.zsh
@@ -0,0 +1,5 @@
+alias f="fd -HI -c always"          
+alias rg="rg --color=always"        
+alias rga="rg --color=auto"         
+
+alias c="cargo"
